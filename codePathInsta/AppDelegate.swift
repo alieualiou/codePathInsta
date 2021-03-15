8aae66d6207e2e52c4fb10bac420ac019250feea
alieualiou <88aliou@gmail.com>
2021-03-15 02:48:11 -0400

    insta

--- codePathInsta/AppDelegate.swift
+++ codePathInsta/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import Parse
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,12 +16,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+       
+        // --- Copy this only
+            
+            let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "HRor7K8A2Reowx6NUVQZt2cG2VstKe39PuBoHBxR" // <- UPDATE
+                    $0.clientKey = "pN5cXGBonh3jlCsoW8jF5xvPUkarw5ZQePVqOze9" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+            
+            // --- end copy
+        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+   func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
